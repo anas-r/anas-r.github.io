da2f8827d52edb0010cb09afb6ad1ab98c7dd02c
anas-r <57984087+anas-r@users.noreply.github.com>
2020-10-21 23:31:26 +0200

    Add files via upload

---
+++ precache-manifest.1a3dbb7918425eef32013dc7e2e817e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a81f5032697fc240782305c84b37bf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11561be3d32f2fb51c4e",
+    "url": "/static/css/main.7bbf261c.chunk.css"
+  },
+  {
+    "revision": "64d6ff8c8146e66f720c",
+    "url": "/static/js/2.850eda74.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/static/js/2.850eda74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11561be3d32f2fb51c4e",
+    "url": "/static/js/main.f34ec413.chunk.js"
+  },
+  {
+    "revision": "ec75d90144033221dce8",
+    "url": "/static/js/runtime-main.0c3778f9.js"
+  },
+  {
+    "revision": "cd786edae5f1dbb67db30b617cd48bc5",
+    "url": "/static/media/me.cd786eda.png"
+  }
+]);
